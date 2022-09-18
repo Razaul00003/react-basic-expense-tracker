bada6ffc50a309b725d859c8a3c7d1a167071a51
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-09-18 18:10:10 +0600

    commit 2 with separate component for all ExpenseItem

--- react1/src/App.js
+++ react1/src/App.js
@@ -15,6 +15,7 @@ function App() {
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
+
     {
       id: "e4",
       title: "New Desk (Wooden)",
@@ -22,7 +23,6 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  console.log("new resutlt loooaded");
 
   return <Expense data={expenses} />;
 }
